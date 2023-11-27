994754b7cadbc8e2abb68c3608d63325cb788865
Mehdi Lagnaoui <100889458+MehdiLagnaoui@users.noreply.github.com>
2023-11-26 23:30:29 -0500

    Create MotFreq.java

---
+++ src/structure/MotFreq.java
@@ -0,0 +1,11 @@
+package enginrecherche.structure;
+
+public class MotFreq {
+    public int frequence;
+    public String mot;
+
+    public MotFreq(String mot,  int frequence) {
+        this.frequence = frequence;
+        this.mot = mot;
+    }
+}
